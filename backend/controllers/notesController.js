7d4fd5d8e921d6799ac191526da395a9d991e9dd
Edwin Roman <romanseberino@gmail.com>
2022-04-03 23:43:48 -0400

    Using Populate method
    Using a version of classics Inner Join of SQL in Mongoose of MongoDB

--- backend/controllers/notesController.js
+++ backend/controllers/notesController.js
@@ -1,27 +1,44 @@
 const { request, response } = require('express');
+const Card = require('../models/schemas/cards');
+const CardCategory = require('../models/schemas/cardsCategories');
+const User = require('../models/schemas/user');
 
-const getAllNotes = (req = request, res = response) =>{
+const getAllNotes = async (req = request, res = response) =>{
 
-    res.json('Getting notes');
+    try {
+            
+        const cards = await Card.find()
+        .populate({
+            path: 'userId',
+            model: 'User',
+            select: 'name lastName'
+        });
+        
+
+        res.status(200).json({ data: [...cards], success: true });
+        
+    } catch (err) {        
+        res.status(500).json({ success: false, msg: err.message });
+    }
 
 }
 
 const insertNotes = (req = request, res = response) =>{
 
-    res.json('Its working...');
+    res.json('Inserting working...');
 
 }
 
 
 const updateNotes = (req = request, res = response) =>{
 
-    res.json('Its working...');
+    res.json('Updating working...');
 
 }
 
 const deleteNotes = (req = request, res = response) =>{
 
-    res.json('Its working...');
+    res.json('Deleting working...');
 
 }
 
